3efbe3b235fb34eabd0d73c7a511944ff71c5cf3
a.23308051280679 <a.23308051280679@cbtis128.edu.mx>
2026-03-24 14:54:50 +0000

    okis

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 A new Flutter project.
 
 ## Getting Started
+## Prompt
+Trabajar en firebase studiogridview  son 2 columnas por 7 filas
+
+en total son 14 imágenes desde GitHub de acuerdo a tu negocio
+
+Cada imagen en la parte inferior tres filas un titulo y un subtitulo y estrellitas  (Mi pagina es de lectura asi que la imformacion va a ser de eso  el fondo va a ser de color negro, la barra va a ser de color azul claro y con el nombre de la app (Lecturas-Andrea Roldan 6-I) en negro y con letra de comic, cada recuadro sera de color azul claro y tendran margen led de color azul obscuro  la info va a ir de color nego y las estrellas un azul diferente al de el color de fondo del recuadro y el led para que se distinga (ESTE ES UN TRABAJO COMPLETAMENTE NUEVO PUEDES HACERLO AQUI SOLO SE USARA EL main.dart)
 
 This project is a starting point for a Flutter application.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,192 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const LecturasApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LecturasApp extends StatelessWidget {
+  const LecturasApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Definimos el estilo de fuente Comic para reutilizarlo
+    const comicTextStyle = TextStyle(
+      fontFamily: 'Comic Sans MS', // Asegúrate de tener esta fuente configurada en pubspec.yaml o usa una similar por defecto del sistema si está disponible
+      color: Colors.black,
+    );
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Lecturas-Andrea Roldan 6-I',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // Fondo de toda la aplicación de color negro
+        scaffoldBackgroundColor: Colors.black,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          // Barra de color azul claro
+          backgroundColor: const Color(0xFFADD8E6), // LightBlue
+          // Nombre de la app en negro y con letra de comic
+          title: const Text(
+            'Lecturas-Andrea Roldan 6-I',
+            style: comicTextStyle,
+          ),
+          centerTitle: true,
+        ),
+        // GridView de 2 columnas por 7 filas (14 imágenes en total)
+        body: GridView.count(
+          crossAxisCount: 2, // 2 columnas
+          // Ajusta la proporción de aspecto para que quepan las 7 filas cómodamente
+          childAspectRatio: 1, // Ajusta este valor según sea necesario para la altura de la fila
+          children: [
+            // Sección de 14 elementos de lectura
+            buildLecturaCard(
+                'El Quijote',
+                'Cervantes',
+                4,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap.JPG'),
+            buildLecturaCard(
+                'Cien años de soledad',
+                'García Márquez',
+                5,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap2.JPG'),
+            buildLecturaCard(
+                '1984',
+                'George Orwell',
+                5,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap3.JPG'),
+            buildLecturaCard(
+                'Fahrenheit 451',
+                'Ray Bradbury',
+                4,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap4.JPG'),
+            buildLecturaCard(
+                'La Odisea',
+                'Homero',
+                5,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap5.JPG'),
+            buildLecturaCard(
+                'El principito',
+                'Saint-Exupéry',
+                5,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap6.JPG'),
+            buildLecturaCard(
+                'Don Juan Tenorio',
+                'Zorrilla',
+                4,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap7.JPG'),
+            buildLecturaCard(
+                'Crónica de una muerte anunciada',
+                'García Márquez',
+                5,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap8.JPG'),
+            buildLecturaCard(
+                'El túnel',
+                'Sábato',
+                4,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap9.JPG'),
+            buildLecturaCard(
+                'Rayuela',
+                'Cortázar',
+                5,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap10.JPG'),
+            buildLecturaCard(
+                'La ciudad y los perros',
+                'Vargas Llosa',
+                4,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap11.JPG'),
+            buildLecturaCard(
+                'Pedro Páramo',
+                'Juan Rulfo',
+                5,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap12.JPG'),
+            buildLecturaCard(
+                'Los miserables',
+                'Víctor Hugo',
+                5,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap13.JPG'),
+            buildLecturaCard(
+                'La metamorfosis',
+                'Kafka',
+                4,
+                'https://raw.githubusercontent.com/RoldanOrtega/UII-Act-5-GridView/refs/heads/main/cap14.JPG'),
+          ],
+        ),
+      ),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  // Función para construir cada recuadro de lectura
+  Widget buildLecturaCard(
+      String titulo, String subtitulo, int estrellas, String imageUrl) {
+    // Definimos el estilo de fuente Comic para la información dentro del recuadro
+    const comicTextStyle = TextStyle(
+      fontFamily: 'Comic Sans MS',
+      color: Colors.black, // Info de color negro
+    );
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Container(
+      // Cada recuadro será de color azul claro
+      color: const Color(0xFFADD8E6), // LightBlue
+      margin: const EdgeInsets.all(8.0), // Margen para separar recuadros
+      child: Container(
+        // Margen led de color azul obscuro
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: const Color(0xFF00008B), // DarkBlue
+            width: 4.0, // Ancho del margen led
+          ),
+        ),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            // Imagen desde GitHub (usando identicons como ejemplo)
+            Expanded(
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.cover, // Ajusta la imagen al recuadro
+              ),
+            ),
+            // Tres filas de información en la parte inferior
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Fila 1: Título
+                  Text(
+                    titulo,
+                    style: comicTextStyle.copyWith(
+                        fontWeight: FontWeight.bold, fontSize: 16.0),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 4.0),
+                  // Fila 2: Subtítulo
+                  Text(
+                    subtitulo,
+                    style: comicTextStyle.copyWith(fontSize: 14.0),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 4.0),
+                  // Fila 3: Estrellitas
+                  Row(
+                    children: List.generate(5, (index) {
+                      return Icon(
+                        index < estrellas ? Icons.star : Icons.star_border,
+                        // Estrellas de un azul diferente al color de fondo y led (ej. azul rey)
+                        color: const Color(0xFF4169E1), // RoyalBlue
+                        size: 20.0,
+                      );
+                    }),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
